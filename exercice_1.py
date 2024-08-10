a9e2f63ecb3a9ec5cfd86447f507b2a6ca88073c
Ilya Dudelzak <65915836+IlyaDudelzak@users.noreply.github.com>
2024-08-10 18:36:46 +0300

    Update exercice_1.py

--- exercice_1.py
+++ exercice_1.py
@@ -4,9 +4,12 @@ def string_to_date(date_string):
   return datetime.strptime(date_string, "%Y-%m-%d").date()
 
 def get_days_from_today(date_):
-
-  today = date.today()
-  delta = today - string_to_date(date_)
-  return delta.days
+  try:
+    today = date.today()
+    delta = today - string_to_date(date_)
+    return delta.days
+  except ValueError:
+    print("Wrong date format!")
+    return 0
 
 print(get_days_from_today("2021-03-05"))
